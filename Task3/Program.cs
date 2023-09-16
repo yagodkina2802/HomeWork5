775301b48595afec56fa23cf0877035247f8c5bf
AnastasiaRekalo <global-rekalonastya@gmail.com>
2023-09-16 19:24:56 +0300

    Решение третьей задачи

---
+++ Task3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+double[] array = new double[5] { 3.22, 4.2, 1.15, 77.15, 65.2 };
+  
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+double MaxEll(double[] arr)
+{
+    
+    double maxx = arr[0];
+    foreach (double ell in arr)
+    {
+        double temp = ell;
+        if(temp > maxx) maxx = temp;
+    }
+    return maxx;
+
+}
+double MinEll(double[] arr)
+{
+
+    double min = arr[0];
+    foreach (double ell in arr)
+    {
+        double temp = ell;
+        if(temp < min) min = temp;
+    }
+    return min;
+
+}
+
+
+
+
+PrintArray(array);
+Console.WriteLine();
+double maxi = MaxEll(array);
+double min = MinEll(array);
+double inequality = maxi - min;
+
+Console.WriteLine($"Разница между максимальным и минимальным значениями массива = {inequality}");
